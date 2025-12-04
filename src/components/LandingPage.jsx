bcc98795186bcc79fd4eff7280b9f62ba85da5c4
Juan Pablo Chong <juan.pablochong@snorkel.ai>
2025-12-04 17:34:12 -0600

    Moving direcotries

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -313,7 +313,7 @@ function LandingPage({ onNavigate }) {
                       Onboarding Video - check #ec-marlin-support-v2 for the password
                     </h4>
   <ProtectedVideo 
-  src="/Marlin-EC-Training/media/videos/turorial-one.mp4"
+  src="/Marlin-EC-Training/turorial-one.mp4"
   password="8bMf%@X+"
 />
 
